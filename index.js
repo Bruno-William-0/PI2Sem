01552a3525b662abf99e5259d12ae9e7bdf4b4ea
Wesley Senciani <wsa.wes05@gmail.com>
2023-05-13 12:19:39 -0300

    AlterandoArquivoIndex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import ClientRouter from './Routes/ClientRoute.js';
 import ConsultController from './Controllers/ConsultController.js';
 import EmployerController from './Controllers/EmployerController.js';
 import ConsultRouter from './Routes/ConsultRoute.js'
-
+import EmployerRouter from './Routes/EmployerRoute.js'
 import PetRouter from './Routes/PetRoute.js';
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use('/client', ClientRouter)
 app.use('/consult', ConsultRouter)
-
+app.use('/employer', EmployerRouter)
 app.use('/pet', PetRouter)
 
 app.listen(3000, () => console.log('API Rodando'))
