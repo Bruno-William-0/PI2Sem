8217439d22f1a14eca17310acbd48707e5cf0313
Bruno-William-0 <Bruwill04@gmail.com>
2023-05-18 19:39:55 -0300

    Client

--- Routes/ClientRoute.js
+++ Routes/ClientRoute.js
@@ -4,6 +4,10 @@ import ClientController from '../Controllers/ClientController.js'
 const ClientRouter = express.Router()
 
 ClientRouter.get('/', ClientController.getClient)
+ClientRouter.post('/', ClientController.createClient)
+ClientRouter.get('/:id', ClientController.getClientById)
+ClientRouter.delete('/:id', ClientController.destroyClient)
+ClientRouter.put('/:id', )
 //ContatoRouter.delete()
 
 export default ClientRouter
