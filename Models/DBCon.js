9e2b58aa31037210bfcb2670ee88908fb023fe51
Bruno-William-0 <Bruwill04@gmail.com>
2023-05-26 21:52:37 -0300

    Banco Elephant

--- Models/DBCon.js
+++ Models/DBCon.js
@@ -1,12 +1,28 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+// const dbConfig = {
+//   user: 'postgres',
+//   password: '123',
+//   host: 'localhost',
+//   port: 5432, // Porta padrão do PostgreSQL é 5432
+//   database: 'postgres',
+// };
+
+// const dbConfig = {
+//   user: 'fatec',
+//   password: 'aYgIY2O1bVgL63wYoFSui2JodHhZYjaG',
+//   host: 'dpg-chlcm5bhp8uej74br2kg-a.oregon-postgres.render.com',
+//   port: 5432, // Porta padrão do PostgreSQL é 5432
+//   database: 'pi_00en',
+// };
+
 const dbConfig = {
-  user: 'fatec',
-  password: 'aYgIY2O1bVgL63wYoFSui2JodHhZYjaG',
-  host: 'dpg-chlcm5bhp8uej74br2kg-a.oregon-postgres.render.com',
+  user: 'ithxcnqj',
+  password: 'YHqVJIwaUY1RdEC6TIEZuufC0z_bkhYk',
+  host: 'silly.db.elephantsql.com',
   port: 5432, // Porta padrão do PostgreSQL é 5432
-  database: 'pi_00en',
+  database: 'ithxcnqj',
 };
 
 
