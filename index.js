c43a050ef601baf0ef30d6e0b3da4523e15e4ed6
Bruno-William-0 <Bruwill04@gmail.com>
2023-05-29 21:24:04 -0300

    Cors atualizado

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-import cors from 'cors'
-import express from 'express'
-import ClientRouter from './Routes/ClientRoute.js';
-// import ConsultController from './Controllers/ConsultController.js';
-// import EmployerController from './Controllers/EmployerController.js';
-import ConsultRouter from './Routes/ConsultRoute.js'
-import EmployerRouter from './Routes/EmployerRoute.js'
-import PetRouter from './Routes/PetRoute.js';
 
-const app = express();
-app.use(express.json());
+    import express from 'express'
+    import cors from 'cors'
+    import ClientRouter from './Routes/ClientRoute.js';
+    // import ConsultController from './Controllers/ConsultController.js';
+    // import EmployerController from './Controllers/EmployerController.js';
+    import ConsultRouter from './Routes/ConsultRoute.js'
+    import EmployerRouter from './Routes/EmployerRoute.js'
+    import PetRouter from './Routes/PetRoute.js';
 
-app.use('/client', ClientRouter)
-app.use('/consult', ConsultRouter)
-app.use('/employer', EmployerRouter)
-app.use('/pet', PetRouter)
+    app.use(cors({
+        origin: '*',
+      }));
+      
+    const app = express();
+    app.use(express.json());
 
-app.listen(3000, () => console.log('API Rodando'))
+    app.use('/client', ClientRouter)
+    app.use('/consult', ConsultRouter)
+    app.use('/employer', EmployerRouter)
+    app.use('/pet', PetRouter)
+
+    app.listen(3000, () => console.log('API Rodando'))
 
